6772fc2cea8706b1553ed0d7241292fd3e06cb67
Talib Aziz <talib.aziz@binaryinformatics.com>
2019-05-02 16:51:35 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,40 @@
+const githubhook = require('githubhook');
+const github = githubhook({
+    secret: 'secret',
+});
+
+github.listen();
+
+github.on('*', function (event, repo, ref, data) {
+    console.log('event========',event);
+	console.log('repo========',repo);
+	console.log('ref========', ref);
+	console.log('data========', repo);
+});
+
+/*
+github.on('event', function (repo, ref, data) {
+});
+
+github.on('event:reponame', function (ref, data) {
+});
+
+github.on('event:reponame:ref', function (data) {
+});
+
+github.on('reponame', function (event, ref, data) {
+});
+
+github.on('reponame:ref', function (event, data) {
+});
+
+// GitLab system hooks
+github.on('*', function (event, type, data) {
+});
+
+github.on('type', function (event, data) {
+});
+
+// if you'd like to programmatically stop listening
+// github.stop();
+*/
